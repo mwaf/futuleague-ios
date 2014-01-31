44d5687e6bbf48ff8930e26b3ea2bc98bd4bc24a
Martin Richter <martin.richter@futurice.com>
2014-01-31 18:10:45 +0100

    Initial commit

---
+++ FutuLeague/FTLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FTLAppDelegate.h
+//  FutuLeague
+//
+//  Created by Martin Richter on 31/01/14.
+//  Copyright (c) 2014 Futurice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FTLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FutuLeague/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FutuLeague
+//
+//  Created by Martin Richter on 31/01/14.
+//  Copyright (c) 2014 Futurice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FTLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FTLAppDelegate class]));
+    }
+}
