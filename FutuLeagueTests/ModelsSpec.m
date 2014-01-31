86d70a07c7d60ed0fdbd3b0c6fb7e8eeb3d4bba9
Evangelos Sismanidis <evangelos.sismanidis@futurice.com>
2014-01-31 20:14:23 +0100

    added TestClass with sample Test to test Models later

---
+++ FutuLeagueTests/ModelsSpec.m
@@ -0,0 +1,21 @@
+//
+//  ModelsSpec.m
+//  FutuLeague
+//
+//  Created by Evangelos Sismanidis on 31/01/14.
+//  Copyright (c) 2014 Futurice. All rights reserved.
+//
+
+#import "Kiwi.h"
+
+SPEC_BEGIN(MathSpec)
+
+describe(@"Math", ^{
+    it(@"is pretty cool", ^{
+        NSUInteger a = 16;
+        NSUInteger b = 26;
+        [[theValue(a + b) should] equal:theValue(42)];
+    });
+});
+
+SPEC_END
